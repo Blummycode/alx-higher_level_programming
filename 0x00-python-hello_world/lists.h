809a707e6c4ee6057db46addf71403aca4fa6fe5
Blummycode <simonresearcher@gmail.com>
2023-03-07 12:22:11 -0800

    check cycle

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -2,12 +2,14 @@
 #define LISTS_H
 
 #include <stdlib.h>
+
 /**
  * struct listint_s - singly linked list
- * @n: interger
+ * @n: integer
  * @next: points to the next node
  *
  * Description: singly linked list node structure
+ * for Holberton project
  */
 typedef struct listint_s
 {
@@ -20,4 +22,5 @@ listint_t *add_nodeint(listint_t **head, const int n);
 void free_listint(listint_t *head);
 int check_cycle(listint_t *list);
 
-#endif
+#endif /* LISTS_H */
+
