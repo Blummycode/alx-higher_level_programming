52cf3e557d3c2bc7cb876854cb5f5732fbe432c0
Blummycode <simonresearcher@gmail.com>
2023-03-13 06:00:01 -0700

    biggest interger

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -8,7 +8,7 @@ def max_integer(my_list=[]):
         return (None)
 
     big = my_list[0]
-    for i in range(len(my_list)):
+    for j in range(len(my_list)):
         if my_list[j] > big:
             big = my_list[j]
 
