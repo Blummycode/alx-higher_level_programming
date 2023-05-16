f0132a97bb12586d7f08db1557aced32963b889b
Blummycode <simonresearcher@gmail.com>
2023-05-16 07:17:05 -0700

    first row

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,3 @@
+-- Inserts a new row into the table first_table in my MySQL server.
+-- Details to be added id = 89, name = Best School 
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Best School");
